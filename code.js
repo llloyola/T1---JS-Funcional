b206b6468e63c6c70a0c33d42b6197111512d168
Luis Loyola <llloyola@uc.cl>
2020-03-24 19:27:34 -0300

    Agregado conversor para línea(s) de código

---
+++ code.js
@@ -0,0 +1,24 @@
+exampleMD = `
+'''javascript
+var s = "JavaScript syntax highlighting";
+alert(s);
+'''
+ 
+'''python
+s = "Python syntax highlighting"
+print s
+'''
+
+'var foo = 0;'
+`
+
+const pipe = functions => data => {
+    return functions.reduce((value, func) => func(value), data);
+}
+
+const pipelineCode = pipe([
+    md => md.replace(/'{3}.+\n([^']+)'{3}/gm, '<code style=display:block;white-space:pre-wrap>\n$1</code>'),
+    md => md.replace(/'([^\n']+)'/g, '<code>$1</code>'),
+]);
+
+console.log(pipelineCode(exampleMD));
