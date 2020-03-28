d86f0a01237465a66bd1f4f5591c3038f2725275
Julian Lires <jlires@uc.cl>
2020-03-27 22:53:23 -0300

    Added list and table conversion to the pipeline.

--- MDtoHTML/listas.mjs
+++ MDtoHTML/listas.mjs
@@ -19,12 +19,10 @@ Juan Lopez:
 
 `
 
-const listMDtoHTML = pipe([
+export const listMDtoHTML = pipe([
     md => md.replace(/(?<=\n|\n\s)\*([^\*\n]+)$/gm, '<li>$1</li>'),
     md => md.replace(/(?<!<\/li>\n)\s(<li>)/gm, '<ul>\n $1'),
     md => md.replace(/(<\/li>)(?!\n\s<li>)/gm, '$1\n</ul>'),
 ]);
 
 //console.log(listMDtoHTML(md));
-
-module.exports = listMDtoHTML;
