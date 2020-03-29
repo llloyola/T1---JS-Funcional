f24fe3a3c268644c33d9ed0d08f4fe9cbb78f8d8
Julian Lires <jlires@uc.cl>
2020-03-29 18:12:53 -0300

    Added minimum amount of lines on header detection

--- MDtoHTML/header.mjs
+++ MDtoHTML/header.mjs
@@ -7,9 +7,9 @@ export const hdMDtoHTML = pipe([
     text => text.replace(/\#{3}(.+)/g, '<h3>$1</h3>'),
     text => text.replace(/\#{2}(.+)/g, '<h2>$1</h2>'),
     text => text.replace(/\#{1}(.+)/g, '<h1>$1</h1>'),
-    text => text.replace(/^(.+)\n\=+/gm, '<h1>$1</h1>'),
-    text => text.replace(/^(.+)\n\-+/gm, '<h2>$1</h2>'),
-    
+    text => text.replace(/^(.+)\n\={2,}/gm, '<h1>$1</h1>'),
+    text => text.replace(/^(.+)\n\-{2,}/gm, '<h2>$1</h2>'),
+
 ]);
 
 export const hdMDtoTXT = pipe([
