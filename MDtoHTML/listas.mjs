12a08fd6c1bc67a4949731d29012e2885dc22f6d
Luis Loyola <llloyola@uc.cl>
2020-03-29 17:48:19 -0300

    Add regex for Ordered Lists

--- MDtoHTML/listas.mjs
+++ MDtoHTML/listas.mjs
@@ -3,9 +3,9 @@ import {pipe} from "./pipeline.mjs";
 
 const md = `
 Luis Loyola:
- * Listas
- * Code and Syntax Highlightning (sin colores)
- * Tablas
+ 1. Listas
+ 2. Code and Syntax Highlightning (sin colores)
+ 3. Tablas
 
 Julian Lires:
  * Emphasis
@@ -23,6 +23,9 @@ export const listMDtoHTML = pipe([
     md => md.replace(/(?<=\n|\n\s)\*([^\*\n]+)$/gm, '<li>$1</li>'),
     md => md.replace(/(?<!<\/li>\n)\s(<li>)/gm, '<ul>\n $1'),
     md => md.replace(/(<\/li>)(?!\n\s<li>)/gm, '$1\n</ul>'),
+    md => md.replace(/(?<=\n|\n\s)(\d\.[^\d\.\n]+)$/gm, '<li>$1</li>'),
+    md => md.replace(/(?<!<\/li>\n|<ul>\n)\s(<li>)/gm, '<ol>\n $1'),
+    md => md.replace(/(<\/li>)(?!\n\s<li>|\n<\/ul>)/gm, '$1\n</ol>'),
 ]);
 
-//console.log(listMDtoHTML(md));
+console.log(listMDtoHTML(md));
