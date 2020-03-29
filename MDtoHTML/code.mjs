147b09b3e4a0417eba7fa347715c45676bad3d7d
Luis Loyola <llloyola@uc.cl>
2020-03-29 19:07:35 -0300

    add .replace() for md to text

--- MDtoHTML/code.mjs
+++ MDtoHTML/code.mjs
@@ -4,3 +4,5 @@ export const cdMDtoHTML = pipe([
     md => md.replace(/`{3}.*\n([^`]+)`{3}/gm, '<code style=display:block;white-space:pre-wrap>\n$1</code>'),
     md => md.replace(/`([^\n`]+)`/g, '<code>$1</code>'),
 ]);
+
+export const cdMDtoTXT = md => md.replace(/^`{3}/g, '');
