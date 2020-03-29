f9c65232622b6e356b539ec6ff6d393084d7769e
Luis Loyola <llloyola@uc.cl>
2020-03-29 18:00:36 -0300

    Delete useless index on ordered lists

--- MDtoHTML/listas.mjs
+++ MDtoHTML/listas.mjs
@@ -23,7 +23,7 @@ export const listMDtoHTML = pipe([
     md => md.replace(/(?<=\n|\n\s)\*([^\*\n]+)$/gm, '<li>$1</li>'),
     md => md.replace(/(?<!<\/li>\n)\s(<li>)/gm, '<ul>\n $1'),
     md => md.replace(/(<\/li>)(?!\n\s<li>)/gm, '$1\n</ul>'),
-    md => md.replace(/(?<=\n|\n\s)(\d\.[^\d\.\n]+)$/gm, '<li>$1</li>'),
+    md => md.replace(/(?<=\n|\n\s)\d\.([^\d\.\n]+)$/gm, '<li>$1</li>'),
     md => md.replace(/(?<!<\/li>\n|<ul>\n)\s(<li>)/gm, '<ol>\n $1'),
     md => md.replace(/(<\/li>)(?!\n\s<li>|\n<\/ul>)/gm, '$1\n</ol>'),
 ]);
